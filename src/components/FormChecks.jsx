dbdc0eff86586d5591638a59ce259b38aaf458a3
Rohini Goyal <goyry006@mymail.unisa.edu.au>
2025-11-11 23:58:44 +1030

    converting formchecks logic into react

--- src/components/FormChecks.jsx
+++ src/components/FormChecks.jsx
@@ -1,15 +1,19 @@
 //implementing this functionality later for Part-B
-function FormChecks() {
+function FormChecks({ checks, onChange }) {
     return (
         <>
             <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="c1" />
+                <input className="form-check-input" type="checkbox" value="" id="c1" checked={checks.c1} onChange={(e) =>
+                    onChange({ ...checks, c1: e.target.checked })} />
+
                 <label className="form-check-label" htmlFor="checkDefault">
                         s1
                     </label>
             </div>
             <div className="form-check">
-                <input className="form-check-input" type="checkbox" value="" id="c2"/>
+                <input className="form-check-input" type="checkbox" value="" id="c2" checked={checks.c2} onChange={(e) =>
+                    onChange({ ...checks, c2: e.target.checked })} />
+
                 <label className="form-check-label" htmlFor="checkChecked">
                         d1
                     </label>
